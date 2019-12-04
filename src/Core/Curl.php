a4972729ecd36a9afc1bb5ad43b8ba543de427ed
心友堂 <667997@gmail.com>
2019-12-04 15:23:47 +0800

    op curl

--- src/Core/Curl.php
+++ src/Core/Curl.php
@@ -78,76 +78,6 @@ abstract class Curl{
 		return $curl;
 	}
 	
-	
-	
-	
-	/**
-	 * 多文件post提交 PHP5.3 ~ PHP 5.4.
-	 * @param resource $ch cURL resource
-	 * @param array $assoc "name => value"
-	 * @param array $files "name => path"
-	 * @return bool
-	 */
-	private static function setCurlPostFields($ch, $assoc = array(), array $files = array()){
-		// invalid characters for "name" and "filename"
-		static $disallow = array("\0", "\"", "\r", "\n");
-		$body = array();
-		
-		// build normal parameters
-		foreach($assoc as $k => $v){
-			$k = str_replace($disallow, "_", $k);
-			$body[] = implode("\r\n", array(
-				"Content-Disposition: form-data; name=\"{$k}\"",
-				"",
-				filter_var($v),
-			));
-		}
-		
-		// build file parameters
-		foreach($files as $k => $v){
-			switch(true){
-				case false === $v = realpath(filter_var($v)):
-				case !is_file($v):
-				case !is_readable($v):
-					continue; // or return false, throw new InvalidArgumentHttpException
-			}
-			$data = file_get_contents($v);
-			$v = call_user_func("end", explode(DIRECTORY_SEPARATOR, $v));
-			$k = str_replace($disallow, "_", $k);
-			$v = str_replace($disallow, "_", $v);
-			$body[] = implode("\r\n", array(
-				"Content-Disposition: form-data; name=\"{$k}\"; filename=\"{$v}\"",
-				"Content-Type: application/octet-stream",
-				"",
-				$data,
-			));
-		}
-		
-		// generate safe boundary
-		do{
-			$boundary = "---------------------".md5(mt_rand().microtime());
-		} while(preg_grep("/{$boundary}/", $body));
-		
-		// add boundary for each parameters
-		array_walk($body, function(&$part) use ($boundary){
-			$part = "--{$boundary}\r\n{$part}";
-		});
-		
-		// add final boundary
-		$body[] = "--{$boundary}--";
-		$body[] = "";
-		
-		// set options
-		return @curl_setopt_array($ch, array(
-			CURLOPT_POST       => true,
-			CURLOPT_POSTFIELDS => implode("\r\n", $body),
-			CURLOPT_HTTPHEADER => array(
-				"Expect: 100-continue",
-				"Content-Type: multipart/form-data; boundary={$boundary}", // change Content-Type
-			),
-		));
-	}
-	
 	/**
 	 * CURL-get方式获取数据
 	 * @param string $url URL
@@ -183,7 +113,7 @@ abstract class Curl{
 	 */
 	public static function post($url, $data, $timeout = self::DEFAULT_TIMEOUT, $curl_option = array()){
 		$opt = array(
-			CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+			CURLOPT_HTTPHEADER     => array('Content-Type: application/json'),
 			CURLOPT_POST           => true,
 			CURLOPT_POSTFIELDS     => json_encode($data),
 			CURLOPT_TIMEOUT        => $timeout,
@@ -214,10 +144,10 @@ abstract class Curl{
 	public static function put($url, $data, $timeout = self::DEFAULT_TIMEOUT, $curl_option = array()){
 		$body = json_encode($data);
 		$opts = array(
-			CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+			CURLOPT_HTTPHEADER    => array('Content-Type: application/json'),
 			CURLOPT_CUSTOMREQUEST => "PUT",
-			CURLOPT_TIMEOUT        => $timeout,
-			CURLOPT_POSTFIELDS => $body
+			CURLOPT_TIMEOUT       => $timeout,
+			CURLOPT_POSTFIELDS    => $body
 		);
 		$curl_option = self::arrayMergeKeepKeys($opts, $curl_option);
 		$curl = self::getCurlInstance($url, $curl_option);
