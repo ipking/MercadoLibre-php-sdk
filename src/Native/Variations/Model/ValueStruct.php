c1a41894a5cb4e1a4f96549ddb95667299a7da27
心友堂 <667997@gmail.com>
2020-05-09 15:43:11 +0800

    add var

---
+++ src/Native/Variations/Model/ValueStruct.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MercadoLibre\Native\Variations\Model;
+
+
+use MercadoLibre\Native\NativeParameter;
+/**
+ * @property string number
+ * @property string unit
+ */
+class ValueStruct extends NativeParameter
+{
+	public function __construct($data = [])
+	{
+		parent::__construct($data, array(
+			'number' => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+			'unit'   => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+		));
+	}
+}
---
+++ src/Native/Variations/Model/Variation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MercadoLibre\Native\Variations\Model;
+
+
+use MercadoLibre\Native\NativeParameter;
+/**
+ * @property string id
+ * @property Attribute[] attribute_combinations
+ * @property Attribute[] attributes
+ * @property array picture_ids
+ * @property string price
+ * @property string available_quantity
+ * @property string seller_custom_field
+ * @property string sold_quantity
+ */
+class Variation extends NativeParameter
+{
+	public function __construct($data = [])
+	{
+		parent::__construct($data, array(
+			'id'                     => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+			'attribute_combinations' => [self::PARAM_TYPE_ARRAY, self::PARAM_OPTIONAL, Attribute::class],
+			'attributes'             => [self::PARAM_TYPE_ARRAY, self::PARAM_ISSET, Attribute::class],
+			'picture_ids'            => [self::PARAM_TYPE_ARRAY, self::PARAM_OPTIONAL, self::PARAM_TYPE_STRING],
+			'price'                  => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+			'available_quantity'     => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+			'seller_custom_field'    => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+			'sold_quantity'          => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
+		));
+	}
+}
