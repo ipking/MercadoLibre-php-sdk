3358eea762c76292e2bac05a1b81358fba2116b7
TK-PC\66799 <667997@gmail.com>
2020-06-23 10:54:40 +0800

    fix mk order

--- src/GlobalSelling/MarketplaceOrdersSearchGet/Model/Order.php
+++ src/GlobalSelling/MarketplaceOrdersSearchGet/Model/Order.php
@@ -6,15 +6,13 @@ use MercadoLibre\Base\BaseParameter;
 
 /**
  * @property string id
- * @property Order[] orders
  */
-class OrderInfo extends BaseParameter
+class Order extends BaseParameter
 {
 	public function __construct($data = [])
 	{
 		parent::__construct($data, array(
-			'id'     => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
-			'orders' => [self::PARAM_TYPE_ARRAY, self::PARAM_OPTIONAL, Order::class],
+			'id' => [self::PARAM_TYPE_STRING, self::PARAM_OPTIONAL],
 		));
 	}
 }
